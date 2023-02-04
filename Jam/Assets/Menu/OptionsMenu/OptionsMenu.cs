7cefa5001d46a89ba338dfff5bf8b332003d84d6
BDTracer <mpungillo@gmail.com>
2023-02-04 23:50:52 +0100

    Menu Scene and scripts - Complete

--- Jam/Assets/Menu/OptionsMenu/OptionsMenu.cs
+++ Jam/Assets/Menu/OptionsMenu/OptionsMenu.cs
@@ -1,22 +1,41 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
+
 
 public class OptionsMenu : MonoBehaviour
 {
     public MenuMng mng;
+    public TextMeshProUGUI brightnessValue;
 
     private void Start()
     {
         mng = GameObject.Find("MenuMng").GetComponent<MenuMng>();
     }
 
-    // Start is called before the first frame update
     public void BackToMainMenu()
     {
         mng.getAudioSource().PlayOneShot(mng.back);
         MenuMng.optionsMenu.SetActive(false);
         MenuMng.mainMenu.SetActive(true);
+    }
 
+    public void IncreaseBrightness()
+    {
+        int index = int.Parse(brightnessValue.text);
+        index++;
+        index = Mathf.Clamp(index, 1, 5);
+        PlayerPrefs.SetInt("Brightness", index);
+        brightnessValue.text = index.ToString();
+    }
+
+    public void DecreaseBrightness()
+    {
+        int index = int.Parse(brightnessValue.text);
+        index--;
+        index = Mathf.Clamp(index, 1, 5);
+        PlayerPrefs.SetInt("Brightness", index);
+        brightnessValue.text = index.ToString();
     }
 }
